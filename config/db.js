e4870baa2723f5e4b69b5bc334cf0705a2249132
RB <gobibi@gmail.com>
2020-04-03 22:42:28 -0400

    Updated temp dev env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 var mysql = require('mysql');
 var pool = "";
 const environment = process.env.NODE_ENV || 'prod';
+// const environment = process.env.NODE_ENV || 'dev';
 
 if (environment === 'dev') {
     pool = mysql.createPool({
         connectionLimit: 100,
         host: '127.0.0.1',
         user: process.env.DB_USERNAME,
-        password: '',
+        password: 'root',
         database: process.env.DB_NAME,
-        debug: false
+        debug: true
     });
 }
 else {
@@ -23,4 +24,3 @@ else {
     });
 }
 module.exports = pool;
-
