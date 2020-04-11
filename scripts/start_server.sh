a7a98eea5d84c8772886b66759562d05fcce6423
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-04-10 23:37:21 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@ export PM2_HOME=/home/ec2-user/.pm2
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo npm install pm2 -g
+npm install pm2 -g
 pm2 delete backend 
 cd /app/backend/back/
 pm2 start --name backend index.js
