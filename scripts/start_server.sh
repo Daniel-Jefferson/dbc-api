357812362e74739ce7c3490523fd9278715b8426
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-04-10 23:16:06 -0500

    Update scripts to initialize the server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
-export PM2_HOME=/home/ec2-user/.pm2  
+export PM2_HOME=/home/ec2-user/.pm2
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 pm2 delete backend 
 cd /app/backend/back/
 pm2 start --name backend index.js
