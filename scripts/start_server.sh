db2bfe5596ea8a86f071cf1873e19d3be7647670
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-04-11 00:27:14 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 touch ~/.bash_profile
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
+pwd
 . ~/.nvm/nvm.sh
 nvm install node
 npm install pm2 -g
