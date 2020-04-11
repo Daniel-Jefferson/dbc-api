4cc10e777f7a10e64f15d16bd45bfefaca0c4a6a
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-04-10 23:58:14 -0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 _#!/bin/bash_
 
-export PM2_HOME=/home/ec2-user/.pm2
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
